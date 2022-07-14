ce60c69d6d023585202f9b2f07221672f50577cc
Karen.Jebakany <karen.jebakany@niit.com>
2022-07-14 14:29:29 +0530

    demo1 added

---
+++ src/main/java/com/jap/poem/PoemAnalyzer.java
@@ -0,0 +1,50 @@
+package com.jap.poem;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PoemAnalyzer {
+    public void readFile(String fileName){
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(fileName);
+            int line = 0;
+            while ((line = fileStream.read()) != -1) {
+                char data = (char) line;
+                System.out.print(data);
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void writeToTheFile(String fileName, String data){
+        byte[] b = data.getBytes();
+        try(FileOutputStream out = new FileOutputStream(fileName))
+        {
+            out.write(b);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        PoemAnalyzer poemAnalyzer = new PoemAnalyzer();
+        String poem = "Behold her, single in the field,\n" +
+                "Yon solitary Highland Lass!\n" +
+                "Reaping and singing by herself;\n" +
+                "Stop here, or gently pass!\n" +
+                "Alone she cuts and binds the grain,\n" +
+                "And sings a melancholy strain;\n" +
+                "O listen! for the Vale profound\n" +
+                "Is overflowing with the sound.";
+        poemAnalyzer.writeToTheFile("D:\\Java-Course-Revamp-Demo-Code\\Course4\\Sprint3\\Demonstrations" +
+                "\\demo-1-poem-analyzer\\demo-1-poem-analyzer\\src\\main\\resources\\poem.txt",poem);
+        poemAnalyzer.readFile("D:\\Java-Course-Revamp-Demo-Code\\Course4\\Sprint3\\Demonstrations" +
+                "\\demo-1-poem-analyzer\\demo-1-poem-analyzer\\src\\main\\resources\\poem.txt");
+
+    }
+}
---
+++ src/test/java/com/jap/poem/PoemAnalyzerTest.java
@@ -0,0 +1,4 @@
+package com.jap.poem;
+
+public class PoemAnalyzerTest {
+}
