26eaed89d52afd5349d07fc0d34f54afc37cbbdb
Pinky Sodhi <pinky.sodhi@niit.com>
2022-09-02 19:12:42 +0530

    chnaged absolute path to relative path in PoemAnalyzer.java

--- src/main/java/com/jap/poem/PoemAnalyzer.java
+++ src/main/java/com/jap/poem/PoemAnalyzer.java
@@ -5,7 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class PoemAnalyzer {
-    public void readFile(String fileName){
+    public static void main(String[] args) {
+        PoemAnalyzer poemAnalyzer = new PoemAnalyzer();
+        String poem = "Behold her, single in the field,\n" + "Yon solitary Highland Lass!\n" + "Reaping and singing by herself;\n" + "Stop here, or gently pass!\n" + "Alone she cuts and binds the grain,\n" + "And sings a melancholy strain;\n" + "O listen! for the Vale profound\n" + "Is overflowing with the sound.";
+        poemAnalyzer.writeToTheFile("src/main/resources/poem.txt", poem);
+        poemAnalyzer.readFile("src/main/resources/poem.txt");
+
+    }
+
+    public void readFile(String fileName) {
         FileInputStream fileStream = null;
         try {
             fileStream = new FileInputStream(fileName);
@@ -14,37 +22,17 @@ public class PoemAnalyzer {
                 char data = (char) line;
                 System.out.print(data);
             }
-        }catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    public void writeToTheFile(String fileName, String data){
+    public void writeToTheFile(String fileName, String data) {
         byte[] b = data.getBytes();
-        try(FileOutputStream out = new FileOutputStream(fileName))
-        {
+        try (FileOutputStream out = new FileOutputStream(fileName)) {
             out.write(b);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-    public static void main(String[] args) {
-        PoemAnalyzer poemAnalyzer = new PoemAnalyzer();
-        String poem = "Behold her, single in the field,\n" +
-                "Yon solitary Highland Lass!\n" +
-                "Reaping and singing by herself;\n" +
-                "Stop here, or gently pass!\n" +
-                "Alone she cuts and binds the grain,\n" +
-                "And sings a melancholy strain;\n" +
-                "O listen! for the Vale profound\n" +
-                "Is overflowing with the sound.";
-        poemAnalyzer.writeToTheFile("D:\\Java-Course-Revamp-Demo-Code\\Course4\\Sprint3\\Demonstrations" +
-                "\\demo-1-poem-analyzer\\demo-1-poem-analyzer\\src\\main\\resources\\poem.txt",poem);
-        poemAnalyzer.readFile("D:\\Java-Course-Revamp-Demo-Code\\Course4\\Sprint3\\Demonstrations" +
-                "\\demo-1-poem-analyzer\\demo-1-poem-analyzer\\src\\main\\resources\\poem.txt");
-
     }
 }
